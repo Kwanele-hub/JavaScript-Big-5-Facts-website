5cc20f032ec65757d451cb21f1ec172c69779fa1
nolwazi749 <nolwazizwane7@gmail.com>
2020-08-21 07:30:34 -0400

    testing audio

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
 
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 10;
+}
     
-    
-
-
-
-
-
-
-$('#content_left').html(html);
-
-// this event listener is triggered when the user clicks on an animal name
-$('.js-animal-name').on('click', function(e){
-    var animal_name = $(e.target).data('id');
-    console.log('rendering detail page for '+animal_name);
-    for (var i=0;i<animals_data.animals.length;i++){
-        var an_animal = animals_data.animals[i];
-        if (an_animal.name == animal_name){
-            break;
-        }
-    }
-    var source   = $("#animal-detail-template").html();
-
-    var template = Handlebars.compile(source);
-
-    var html    = template(an_animal);
-
-    $('#content_right').html(html);
-
-
-});
+ 
